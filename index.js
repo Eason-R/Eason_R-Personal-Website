1ce5c8caeaa569de51d5ad4944703408362bbe15
Eason_R <rys582132403@163.com>
2023-05-04 20:04:01 +0800

    init

---
+++ index.js
@@ -0,0 +1,118 @@
+// 导入 express 模块 
+const express = require('express');
+// create express server instance 
+const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
+// static resource router
+// serve css as static
+app.use(express.static(__dirname + "/public"));
+
+// HelloWorld
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    console.log(__dirname)
+});
+
+// Intro Page
+app.get("/home", (req,res) => {
+    res.sendFile(__dirname + "/public/pages/Intro.html");
+})
+
+// About Page
+app.get("/about", (req,res) => {
+    res.sendFile(__dirname + "/public/pages/Detail.html");
+})
+
+app.get("/test", (req,res) => {
+    res.sendFile(__dirname+'/public/pages/WebTest.html')
+})
+
+// Chat Page
+app.get("/chat", (req,res) => {
+    res.sendFile(__dirname + "/public/pages/Chat.html");
+})
+
+
+// request handler
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+let users = {};
+let onlineUsers = [];
+
+io.on('connection',function(socket){
+
+    socket.on('new user', (name) => {
+        // console.log("receive login request"
+        let isnew = false;
+        if(!(onlineUsers.includes(name))){
+            isnew = !isnew
+            users[name] = socket  // save new user as private socket  key{name}=value{socket}
+            onlineUsers.push(name)  // save username to online user list
+            console.log(onlineUsers)
+        }
+        io.emit('online users', isnew,onlineUsers)// brocast online user list
+    });
+
+    // monitor private message
+    socket.on('private message', (from,to,msg) => {
+        if(to in users){ 
+            users[to].emit('to'+to, {msg,from,to}); 
+        }
+    });
+
+    // monitor public message
+    socket.on('public message', (from,msg) => {
+        let time = new Date();
+        let message = {
+            user: from,
+            msg: msg,
+            time: time.toLocaleString()
+        }
+        io.emit('new message', message)
+    });
+
+    // brocast typing user
+    socket.on('typing', user => {
+        io.emit('typing', user)
+    });
+
+    // monitor user exit
+    socket.on('disconnect', () => {
+        let logoutUserName=Object.keys(users).find(key=>users[key]==socket);
+        if(logoutUserName != undefined){
+            delete users[logoutUserName];
+        }
+        onlineUsers = onlineUsers.filter(user => user!=logoutUserName);
+        console.log(onlineUsers);
+        io.emit('user disconnected',logoutUserName,onlineUsers);
+    });
+});
+
+router.post("/login", (req,res)=>{
+    let username = req.body.name,isnew = false;
+    if(!(onlineUsers.includes(username))){
+        isnew = !isnew
+        users[username] = req.body.socket  // save new user as private socket  key{username}=value{socket}
+        onlineUsers.push(username)  // save username to online user list
+        console.log(onlineUsers)
+    }
+    io.emit('online users', isnew,onlineUsers)// brocast online user list
+});
+
+app.use("/",router);
+
+// listen backend port
+http.listen(8888,() => {
+    console.log('server is running at http://127.0.0.1:8888') 
+});
+
+// 调用 app.listen 方法，指定端口号并启动web服务器 
+// app.listen(8888, function () {   
+//     console.log('server is running at http://127.0.0.1:8888') 
+// })
+
---
+++ readme.md
@@ -0,0 +1,36 @@
+# ABD-web assignment Report
+Author: Yusheng Ren
+
+Date: 04/2023
+## How to run the program
+if you are in Windows
+```shell
+cd ... #cd to project base path
+npm i
+nodemon
+```
+Then you can access `localhost:8888/` to visit pages by any browser.
+(Chorme or Edge are recommened)
+
+## Design
+### Introduction Page
+I made it a simple style hello page.
+
+In the bottom part of the page, a set of navigation can direct user to all 3 main pages.
+
+And I added text and wave animation to make the page more interesting.
+### About Page
+This page contains some of my informations. The header of the page is a post that make the page looks nice. And the main area is divided into two parts. The larger one is where the main text are in while another is navigation buttons and my comment.
+### Chat application
+The chat application is the only function that is not static and requires communication with back end.
+## Challenges
+
+## Web Communication
+
+## References
+ 1. [Best Personal Websites: 35+ Examples to Blow Your Mind](https://blog.hubspot.com/marketing/best-personal-websites)
+ 2. [Bootstrap v4 Docs - Simplified Chinese](https://v4.bootcss.com/docs/getting-started/introduction/)
+ 3. [Seán Halpin — Product Designer](https://www.seanhalpin.xyz/)
+ 4. [CSS教程](https://www.w3school.com.cn/css/index.asp)
+ 5. [CSDN博客_前端必学的CSS3波浪效果演示_国服第二切图仔的博客](https://blog.csdn.net/m0_61243965/article/details/125268823)
+ 6. ChatGPT
